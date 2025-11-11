9aa92ceaef7029315f0402e4b65bc4ace5e8d209
AlonRamirez <alonsormz.mail@gmail.com>
2025-11-11 14:09:39 -0600

    agregar las pantallas del menu principal

---
+++ app/src/main/java/com/example/carrentv1/Routes.kt
@@ -0,0 +1,12 @@
+package com.example.carrentv1
+
+object Routes {
+    var screenAgregar = "screenAgregar"
+    var screenSoporte = "screenSoporte"
+    var screenContacto = "screenContacto"
+    var screenPerfil = "screenPerfil"
+    var screenMasinfo = "screenMasinfo"
+    var screenCerrar = "screenCerrar"
+    var screenInicio = "screenInicio"
+    var screenAuto = "screenAuto"
+}
